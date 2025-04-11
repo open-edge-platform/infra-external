e3e37885131eeb9ca54b2576097847b0c16398ae
Ihor Kyrychok <ihor.kyrychok@intel.com>
2025-04-11 17:55:06 +0200

    AMT init (#11)
    * AMT init
    
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>
    
    * Update CI
    
    * Fix lint
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>
    
    * Go mod
    
    * Remove test
    
    * Fix CI
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>
    
    * Buildable docker container
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>
    
    * fix lint
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>
    
    * add file to pkg
    
    * License
    
    ---------
    
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>

---
+++ amt-manager/cmd/amt-manager.go
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"context"
+
+	"github.com/open-edge-platform/orch-library/go/pkg/controller/v2"
+)
+
+func main() {
+}
+
+type ID string
+
+func (id ID) String() string {
+	return "12345678"
+}
+
+func SampleReconcile(_ context.Context, request controller.Request[ID]) controller.Directive[ID] {
+	return request.Ack()
+}
