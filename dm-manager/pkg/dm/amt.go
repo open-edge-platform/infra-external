9bdb5dda73c1f851ef6f9a3463f9c3334ed0d81c
Ihor Kyrychok <ihor.kyrychok@intel.com>
2025-04-25 13:42:19 +0200

    Swagger gen for DM (#34)
    Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>

---
+++ dm-manager/pkg/dm/amt.go
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+package dm
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/open-edge-platform/infra-core/inventory/v2/pkg/logging"
+	"github.com/open-edge-platform/infra-external/dm-manager/pkg/api"
+)
+
+var log = logging.GetLogger("DmReconciler")
+
+type Reconciler struct {
+	APIClient api.ClientWithResponsesInterface
+	TermChan  chan bool
+	ReadyChan chan bool
+	WaitGroup *sync.WaitGroup
+}
+
+func (dmr *Reconciler) Start() {
+	ticker := time.NewTicker(time.Minute)
+	if dmr.ReadyChan != nil {
+		dmr.ReadyChan <- true
+	}
+	log.Info().Msgf("Starting periodic reconciliation")
+	dmr.Reconcile(context.Background())
+	for {
+		select {
+		case <-ticker.C:
+			log.Info().Msgf("Running periodic reconciliation")
+			dmr.Reconcile(context.Background())
+		case <-dmr.TermChan:
+			log.Info().Msgf("Stopping periodic reconciliation")
+			ticker.Stop()
+			dmr.WaitGroup.Done()
+			return
+		}
+	}
+}
+
+func (dmr *Reconciler) Stop() {
+}
+
+func (dmr *Reconciler) Reconcile(ctx context.Context) {
+	devicesRsp, err := dmr.APIClient.GetApiV1DevicesWithResponse(ctx,
+		&api.GetApiV1DevicesParams{})
+	if err != nil {
+		log.Err(err).Msgf("cannot get devices")
+		return
+	}
+
+	log.Info().Msgf("devices - %s", string(devicesRsp.Body))
+
+	for _, device := range *devicesRsp.JSON200 {
+		resp, err := dmr.APIClient.PostApiV1AmtPowerActionGuidWithResponse(ctx, *device.Guid,
+			api.PostApiV1AmtPowerActionGuidJSONRequestBody{
+				Action: api.PowerActionRequestActionN10, // reset
+			})
+		if err != nil {
+			log.Err(err).Msgf("cannot reset %v device", *device.Guid)
+			return
+		}
+		log.Info().Msgf("reset %v device - %s", *device.Guid, string(resp.Body))
+	}
+}
