18a37309ef6977c3dc8609d29328066d8631a1af
Ihor Kyrychok <ihor.kyrychok@Intel.com>
2025-05-27 15:48:50 +0200

    Lint Signed-off-by: Ihor Kyrychok <ihor.kyrychok@intel.com>

--- dm-manager/pkg/dm/utils_test.go
+++ dm-manager/pkg/dm/utils_test.go
@@ -1,7 +1,5 @@
-/*
- * // SPDX-FileCopyrightText: (C) 2025 Intel Corporation
- * // SPDX-License-Identifier: Apache-2.0
- */
+// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 
 package dm
 
