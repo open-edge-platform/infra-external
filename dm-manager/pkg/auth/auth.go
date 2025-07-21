85eb94efe2db4075bcd9ccfd9ad3b730d4ce05de
cjnolan <christopher.nolan@intel.com>
2025-07-21 16:39:40 +0000

    Remove debug messages

--- dm-manager/pkg/auth/auth.go
+++ dm-manager/pkg/auth/auth.go
@@ -5,7 +5,6 @@ package auth
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"strconv"
 
@@ -18,8 +17,7 @@ func GetToken(ctx context.Context) (context.Context, error) {
 	requireTokenStr := os.Getenv("USE_M2M_TOKEN")
 	requireToken, err := strconv.ParseBool(requireTokenStr)
 	if err != nil || !requireToken {
-		fmt.Printf("USE_M2M_TOKEN not set")
-		return ctx, fmt.Errorf("environment variable not set")
+		return ctx, nil //nolint: nilerr // no need to return error if USE_M2M_TOKEN is not set, just skip token retrieval
 	}
 	keycloakServer := os.Getenv("KEYCLOAK_SERVER")
 	vaultServer := os.Getenv("VAULT_SERVER")
@@ -38,8 +36,7 @@ func GetToken(ctx context.Context) (context.Context, error) {
 		return ctx, err
 	}
 	if tokenStr == "" {
-		fmt.Printf("tokenStr empty")
-		return ctx, fmt.Errorf("tokenStr empty")
+		return ctx, nil
 	}
 
 	updatedCtx := context.WithValue(ctx, ContextValue("Authorization"), "Bearer "+tokenStr)
