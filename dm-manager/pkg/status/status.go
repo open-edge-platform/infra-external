9f176a5411ce8c3232a45654344b818558b4cb7c
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>
2025-08-01 17:50:46 +0530

    Fix lint errors
    Signed-off-by: Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>

--- dm-manager/pkg/status/status.go
+++ dm-manager/pkg/status/status.go
@@ -12,14 +12,16 @@ var (
 
 	// Resource statuses for AMT activation.
 	AMTActivationStatusUnknown    = inv_status.New("Unknown", statusv1.StatusIndication_STATUS_INDICATION_UNSPECIFIED)
-	AMTActivationStatusInProgress = inv_status.New("AMT Activation In Progress", statusv1.StatusIndication_STATUS_INDICATION_IN_PROGRESS)
-	AMTActivationStatusFailed     = inv_status.New("AMT Activation Failed", statusv1.StatusIndication_STATUS_INDICATION_ERROR)
-	AMTActivationStatusDone       = inv_status.New("AMT Activation Done", statusv1.StatusIndication_STATUS_INDICATION_IDLE)
+	AMTActivationStatusInProgress = inv_status.New("AMT Activation In Progress",
+		statusv1.StatusIndication_STATUS_INDICATION_IN_PROGRESS)
+	AMTActivationStatusFailed = inv_status.New("AMT Activation Failed", statusv1.StatusIndication_STATUS_INDICATION_ERROR)
+	AMTActivationStatusDone   = inv_status.New("AMT Activation Done", statusv1.StatusIndication_STATUS_INDICATION_IDLE)
 
 	// Resource statuses for AMT deactivation.
 	AMTDeactivationStatusFailed     = inv_status.New("AMT Deactivation Failed", statusv1.StatusIndication_STATUS_INDICATION_ERROR)
 	AMTDeactivationStatusDone       = inv_status.New("AMT Deactivation Done", statusv1.StatusIndication_STATUS_INDICATION_IDLE)
-	AMTDeactivationStatusInProgress = inv_status.New("AMT Deactivation In Progress", statusv1.StatusIndication_STATUS_INDICATION_IN_PROGRESS)
+	AMTDeactivationStatusInProgress = inv_status.New("AMT Deactivation In Progress",
+		statusv1.StatusIndication_STATUS_INDICATION_IN_PROGRESS)
 
 	// Resource statuses for AMT status.
 	AMTStatusUnknown  = inv_status.New("AMT Status Unknown", statusv1.StatusIndication_STATUS_INDICATION_UNSPECIFIED)
