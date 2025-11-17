f97e8803490c2e282583f0fde6dc184b8944dbba
jagratac <jagrat.acharya@intel.com>
2025-11-17 03:15:20 +0000

    Vpro detailes changes

--- dm-manager/go.mod
+++ dm-manager/go.mod
@@ -8,7 +8,7 @@ go 1.24.6
 require (
 	github.com/google/uuid v1.6.0
 	github.com/oapi-codegen/runtime v1.1.1
-	github.com/open-edge-platform/infra-core/inventory/v2 v2.31.0
+	github.com/open-edge-platform/infra-core/inventory/v2 v2.32.0
 	github.com/open-edge-platform/infra-external/loca-onboarding/v2 v2.19.0
 	github.com/open-edge-platform/orch-library/go v0.6.1
 	github.com/prometheus/client_golang v1.23.2
--- dm-manager/pkg/tenant/utils.go
+++ dm-manager/pkg/tenant/utils.go
@@ -69,8 +69,8 @@ func Ptr[T any](v T) *T {
 
 func clientCallback() func(ctx context.Context, req *http.Request) error {
 	callbackFunc := func(ctx context.Context, req *http.Request) error {
-		type headerValue string
-		tenantID, ok := ctx.Value(headerValue("tenantId")).(string)
+		tenantID, ok := ctx.Value(contextValue("tenantId")).(string)
+		log.Info().Msgf("JAGRAT### TenantID from utils: %v", tenantID)
 		if ok {
 			req.Header.Add("ActiveProjectId", tenantID)
 		}
